f84871dd1bf2901876efda0cd090ee453a7450e5
Arvinder Singh Kandola <askandola07@gmail.com>
2022-07-15 17:01:09 +0530

    Modified teamName field for exported participant model file

--- escalade_backend/registration/admin.py
+++ escalade_backend/registration/admin.py
@@ -36,6 +36,7 @@ admin.site.register(Team, TeamAdmin)
 class ParticipantResource(resources.ModelResource):
     class Meta:
         model = Participant
+        fields = ('id', 'name', 'email', 'roll_no', 'discord_ID', 'phone_no', 'team__teamName')
 
 class ParticipantAdmin(ImportExportModelAdmin):
     resource_class = ParticipantResource
